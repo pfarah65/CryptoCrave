91f83a5c5a8243e145562461c951edb1a057b010
pfara039 <pfara039@uottwa.ca>
2017-12-23 18:53:56 -0500

    added gem devise

--- Gemfile
+++ Gemfile
@@ -18,10 +18,11 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-#gem bootstrap - PF
+#gem bootstrap,friendlyId and devise authentication - PF
 gem 'bootstrap', '4.0.0.alpha3'
 #gem friendlyId
 gem 'friendly_id', ' ~> 5.1.0'
+gem 'devise'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-	get "about" => 'pages#about', as: :about
+
+	root to: 'blog/posts#index'
+
+	#/author/....
+	namespace :author do
+  		resources :posts
+	end
+
+	# does not expect author to go to these links
+	scope module: 'blog' do
+		get "about" => 'pages#about', as: :about
 		get "contact" => 'pages#contact', as: :contact
-	root to: 'posts#index'
-  resources :posts
+		get "posts" => 'posts#index' , as: :posts
+		get "posts/:id" => 'posts#show', as: :post
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
