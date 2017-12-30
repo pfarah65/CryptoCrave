81a7eadac8d7dfa0785eb7a95abd1db1d2b5601b
pfara039 <pfara039@uottwa.ca>
2017-12-30 00:04:14 -0500

    Basic login page implemented

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,8 @@ Rails.application.routes.draw do
 		get "contact" => 'pages#contact', as: :contact
 		get "posts" => 'posts#index' , as: :posts
 		get "posts/:id" => 'posts#show', as: :post
-		post "posts/search/" => 'posts#search', as: :search_posts
+		#post "search" => 'posts#search', as: :search_posts
+		get "search" => 'posts#search', as: :search_posts
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
